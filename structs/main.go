86a06e5e68979a5e501549898d4861e89913bb16
Navid Rahman <navidrahman92@gmail.com>
2025-09-19 22:44:42 +0600

    added struct examples

---
+++ structs/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+}
+
+func main() {
+	// 1st way of defining struct
+	navid := person{"Navid", "Rahman"}
+	fmt.Println(navid)
+
+	// 2nd way of defining struct
+	seyam := person{firstName: "Sadman", lastName: "Seyam"}
+	fmt.Println(seyam)
+
+	// 3rd way of defining struct
+	var fatin person
+	fmt.Println(fatin)
+	fmt.Printf("%+v", fatin)
+}
